b010fbb526cd074736d226c40851c9f4c9dda7cd
jiarong.tian <jiarong.tian@hzlt3.com>
2019-06-20 09:53:23 +0800

    go学习

--- example/monkey.go
+++ example/monkey.go
@@ -6,8 +6,16 @@ import (
 	"work/opgo/example/xpp"
 )
 
-func main() {
+/*
+猴子补丁
+在测试用例中挺好用的：https://github.com/bouk/monkey。注意下载和引入是`bou.ke/monkey`，github地址只是放的代码，而不是引入用的地址。
 
+参考：https://www.jianshu.com/p/2f675d5e334e?utm_campaign=maleskine&utm_content=note&utm_medium=seo_notes&utm_source=recommendation
+
+但有时候可能不生效，比如有内敛优化的时候，此时可以加上-gcflags=-l禁用内敛优化来测试，但也未必会生效。
+*/
+
+func main() {
 	p := monkey.Patch(xpp.GetName, func(id int) string {
 		return "替换值"
 	})
---
+++ study/blog/http_proxy.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"log"
+	"net"
+	"net/url"
+	"strings"
+)
+
+func main() {
+	listen, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	for {
+		conn, err := listen.Accept()
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		go handleConn(conn)
+	}
+}
+
+func handleConn(conn net.Conn) {
+	if conn == nil {
+		return
+	}
+	defer conn.Close()
+
+	buf := make([]byte, 1024)
+
+	n, err := conn.Read(buf)
+	if err != nil {
+		fmt.Printf("err: %s\n", err.Error())
+		return
+	}
+	fmt.Printf("[%d]: %s\n\n", n, string(buf[:n]))
+	var method, host, address string
+	fmt.Sscanf(string(buf[:bytes.IndexByte(buf[:], '\n')]), "%s%s", &method, &host)
+
+	hostPortUrl, err := url.Parse(host)
+	if err != nil {
+		fmt.Printf("parse err: %s\n", err.Error())
+		return
+	}
+
+	if hostPortUrl.Opaque == "443" { // https访问
+		address = hostPortUrl.Scheme + ":443"
+	} else { // http访问
+		if strings.Index(hostPortUrl.Host, ":") == -1 { // host不带端口，默认80
+			address = hostPortUrl.Host + ":80"
+		} else {
+			address = hostPortUrl.Host
+		}
+	}
+	fmt.Println(method, host, address)
+
+	//获得了请求的host和port，就开始拨号吧
+	server, err := net.Dial("tcp", address)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	if method == "CONNECT" {
+		fmt.Fprint(conn, "HTTP/1.1 200 Connection established\r\n\r\n")
+	} else {
+		server.Write(buf[:n])
+	}
+	//进行转发
+	go io.Copy(server, conn)
+	io.Copy(conn, server)
+}
