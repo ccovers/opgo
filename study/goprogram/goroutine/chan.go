c63157cb814117eba14a01a2f570a42463896b2b
jiarong.tian <jiarong.tian@hzlt3.com>
2019-07-18 11:38:17 +0800

    回声测试

---
+++ study/goprogram/goroutine/chan.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net"
+	"os"
+)
+
+// 通道
+func main() {
+	// struct{}类型的channel，它不能被写入任何数据，只有通过close()函数进行关闭操作，不占用任何内存！
+	done := make(chan struct{})
+	// ch := make(chan int)     // 无缓冲通道
+	// ch1 := make(chan int, 0) // 无缓冲通道
+	// ch2 := make(chan int, 3) // 缓冲通道
+
+	conn, err := net.Dial("tcp", "localhost:8000")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	go func() {
+		io.Copy(os.Stdout, conn)
+		fmt.Println(done)
+		close(done)
+	}()
+	mustCopy(conn, os.Stdin)
+	conn.Close()
+	<-done
+}
+
+func mustCopy(dst io.Writer, src io.Reader) {
+	if _, err := io.Copy(dst, src); err != nil {
+		fmt.Println(err)
+	}
+}
