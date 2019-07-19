4eaea1af3ef1e1816ca6b46378c8540e396efa04
jiarong.tian <jiarong.tian@hzlt3.com>
2019-07-19 10:01:05 +0800

    改进客户端退出

--- study/goprogram/goroutine/netcat.go
+++ study/goprogram/goroutine/netcat.go
@@ -25,7 +25,24 @@ func main() {
 }
 
 func mustCopy(dst io.Writer, src io.Reader) {
-	if _, err := io.Copy(dst, src); err != nil {
+	/*if _, err := io.Copy(dst, src); err != nil {
 		log.Fatal(err)
+	}*/
+	for {
+		buf := make([]bye, 1024)
+		_, err := src.Read(p)
+		if err != nil {
+			fmt.Println("read:", err)
+			break
+		}
+		if string(buf) == "exit" {
+			fmt.Println("eixt!")
+			break
+		}
+		_, err = dst.Write(buf)
+		if err != nil {
+			fmt.Println("write:", err)
+			break
+		}
 	}
 }
