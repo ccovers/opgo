99309be2dd0db6a3791160ecbb97f076859d3f10
jiarong.tian <jiarong.tian@hzlt3.com>
2019-10-28 17:16:57 +0800

    mysql

--- mysql/main.go
+++ mysql/main.go
@@ -2,9 +2,15 @@ package main
 
 import (
 	"fmt"
-	_ "github.com/siddontang/go-mysql"
+	_ "github.com/siddontang/go-mysql/mysql"
 )
 
 func main() {
-
+	// fmt.Println("===")
+	// mysql.ParseMariadbGTID()
+	if a := 1; false {
+	} else if b := 2; false {
+	} else {
+		fmt.Println(a, b)
+	}
 }
