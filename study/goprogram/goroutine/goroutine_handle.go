7d38dfc558ec8810e43bed2bdc798c473262dc78
jiarong.tian <jiarong.tian@hzlt3.com>
2019-12-23 20:49:35 +0800

    gorpitine 框架

--- study/goprogram/goroutine/goroutine_handle.go
+++ study/goprogram/goroutine/goroutine_handle.go
@@ -6,46 +6,42 @@ import (
 	"time"
 )
 
-func main() {
-	data := []int{1, 2, 3, 4, 5}
+type HanleParam struct {
+	Data interface{}
+	Num  int
+	Mx   *sync.Mutex
+}
 
-	var mx sync.Mutex
-	queue := make(chan struct{}, 5) // 限制同时最多开启5个协程
-	signal := make(chan struct{})   // 通知请求完成
+func GoHandle(goNum int, handle func(*HanleParam, int), param *HanleParam) {
+	queue := make(chan struct{}, goNum) // 限制同时最多开启 goNum 个协程
+	signal := make(chan struct{})       // 通知请求完成
 	defer close(queue)
 	defer close(signal)
 
-	fmt.Println("=====\n")
-	for _, v := range data {
+	for i := 0; i < param.Num; i++ {
 		// 每个数据创建协程处理时向chanel中写数据，超过限制的协程序数量时，阻塞
 		queue <- struct{}{}
 
-		go func(v int) {
-			fmt.Printf("%d \n", v)
-
-			// 处理数据时，加锁
-			mx.Lock()
-			time.Sleep(1 * time.Second)
-			fmt.Println(v, ":", getNum())
-			fmt.Printf("%d ...\n", v)
-			mx.Unlock()
-
+		go func(param *HanleParam, index int) {
+			// 处理数据
+			handle(param, index)
 			// 协程处理完毕，释放限制
 			<-queue
 			// 处理完成通知
 			signal <- struct{}{}
-		}(v)
+		}(param, i)
 	}
 
 	// 数据处理通知，统计数量达成所有数据，则跳出循环
 	cnt := 0
-	for range signal {
-		cnt += 1
-		if cnt == len(data) {
-			break
+	if cnt < param.Num {
+		for range signal {
+			cnt += 1
+			if cnt == param.Num {
+				break
+			}
 		}
 	}
-	fmt.Println("=====...\n")
 }
 
 var num int
@@ -53,4 +49,25 @@ var num int
 func getNum() int {
 	num += 1
 	return num
+}
+
+func main() {
+	data := []int{1, 2, 3, 4, 5}
+
+	fmt.Println("=====\n")
+	GoHandle(2, handle, &HanleParam{Data: data, Num: len(data), Mx: new(sync.Mutex)})
+	fmt.Println("=====...\n")
+}
+
+func handle(param *HanleParam, index int) {
+	data, ok := param.Data.([]int)
+	if !ok {
+		return
+	}
+	time.Sleep(1 * time.Second)
+
+	// 处理数据时，加锁
+	param.Mx.Lock()
+	fmt.Println(data[index], ":", getNum())
+	param.Mx.Unlock()
 }
--- test.go
+++ test.go
@@ -2,41 +2,47 @@ package main
 
 import (
 	"fmt"
+	"strings"
 	"sync"
 	"time"
 )
 
-func main() {
-	var mx sync.Mutex
-	data := []int{1, 2, 3, 4, 5}
-	queue := make(chan struct{}, 2)
-	signal := make(chan struct{})
+type HanleParam struct {
+	Data interface{}
+	Num  int
+	Mx   *sync.Mutex
+}
+
+func GoHandle(goNum int, handle func(*HanleParam, int), param *HanleParam) {
+	queue := make(chan struct{}, goNum) // 限制同时最多开启 goNum 个协程
+	signal := make(chan struct{})       // 通知请求完成
 	defer close(queue)
 	defer close(signal)
 
-	fmt.Println("=====\n")
-	for _, v := range data {
+	for i := 0; i < param.Num; i++ {
+		// 每个数据创建协程处理时向chanel中写数据，超过限制的协程序数量时，阻塞
 		queue <- struct{}{}
-		go func(v int) {
-			fmt.Printf("%d \n", v)
-			time.Sleep(1 * time.Second)
-			mx.Lock()
-			fmt.Println(v, ":", getNum())
-			mx.Unlock()
-			fmt.Printf("%d ...\n", v)
+
+		go func(param *HanleParam, index int) {
+			// 处理数据
+			handle(param, index)
+			// 协程处理完毕，释放限制
 			<-queue
+			// 处理完成通知
 			signal <- struct{}{}
-		}(v)
+		}(param, i)
 	}
 
+	// 数据处理通知，统计数量达成所有数据，则跳出循环
 	cnt := 0
-	for range signal {
-		cnt += 1
-		if cnt == len(data) {
-			break
+	if cnt < param.Num {
+		for range signal {
+			cnt += 1
+			if cnt == param.Num {
+				break
+			}
 		}
 	}
-	fmt.Println("=====...\n")
 }
 
 var num int
@@ -44,4 +50,28 @@ var num int
 func getNum() int {
 	num += 1
 	return num
+}
+
+func main() {
+	fmt.Println(strings.Contains("asdjklll", "djo"))
+	return
+
+	data := []int{1, 2, 3, 4, 5}
+
+	fmt.Println("=====\n")
+	GoHandle(2, handle, &HanleParam{Data: data, Num: len(data), Mx: new(sync.Mutex)})
+	fmt.Println("=====...\n")
+}
+
+func handle(param *HanleParam, index int) {
+	data, ok := param.Data.([]int)
+	if !ok {
+		return
+	}
+	time.Sleep(1 * time.Second)
+
+	// 处理数据时，加锁
+	param.Mx.Lock()
+	fmt.Println(data[index], ":", getNum())
+	param.Mx.Unlock()
 }
