8af962221d2ce547b3475af1c8ec080baf289a6d
jiarong.tian <jiarong.tian@axinan.com>
2020-07-16 18:35:52 +0800

    new algrithm

--- main.go
+++ main.go
@@ -4,33 +4,37 @@ import (
 	"fmt"
 )
 
-func main() {
-	fmt.Println("=====", maxProfit([]int{1, 2, -10, 0, 2}))
+//  Definition for a binary tree node.
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
 }
 
-func maxProfit(prices []int) int {
-	if len(prices) == 0 {
+func getNode(pos int, nums []int) *TreeNode {
+	if pos < len(nums) && nums[pos] != 0xFFFFFFFF {
+		return &TreeNode{
+			Val:   nums[pos],
+			Left:  getNode(2*pos+1, nums),
+			Right: getNode(2*pos+2, nums),
+		}
+	}
+	return nil
+}
+
+func main() {
+	node := getNode(0, []int{1, 2, 3, 4, 0xFFFFFFFF, 0xFFFFFFFF, 5})
+	x := maxDepth(node)
+	fmt.Println("===", x)
+}
+func maxDepth(root *TreeNode) int {
+	if root == nil {
 		return 0
 	}
-
-	dp := make([][]int, len(prices))
-	for i := range dp {
-		dp[i] = make([]int, 3)
+	x := maxDepth(root.Left)
+	y := maxDepth(root.Right)
+	if x > y {
+		return x + 1
 	}
-	dp[0][0] = 0          // 不持股
-	dp[0][1] = -prices[0] // 持股
-	dp[0][2] = 0          // 冷冻期
-	for i := 1; i < len(prices); i++ {
-		dp[i][0] = max(dp[i-1][0], dp[i-1][2])
-		dp[i][1] = max(dp[i-1][1], dp[i-1][0]-prices[i])
-		dp[i][2] = dp[i-1][1] + prices[i]
-	}
-	return max(dp[len(prices)-1][0], dp[len(prices)-1][2])
-}
-
-func max(a, b int) int {
-	if a > b {
-		return a
-	}
-	return b
+	return y + 1
 }
