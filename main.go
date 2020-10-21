1c40a1eccb6ce7233a7989fa76f3496dfa6120c6
jiarong <15982195424@163.com>
2020-10-21 21:01:49 +0800

    delete extra file

--- main.go
+++ main.go
@@ -16,10 +16,15 @@ type TBase struct {
     Gard string
 }
 
+type TKey struct {
+    Id   int32
+    Name string
+}
+
 type TGrid struct {
     X       int32
     Y       int32
-    NMap    map[int32]*TBase
+    NMap    map[TKey]*TBase
     Array   []int32
     Unkonwn interface{}
 }
@@ -28,7 +33,7 @@ func main() {
     grid := &TGrid{
         X:       1,
         Y:       1,
-        NMap:    map[int32]*TBase{1: &TBase{Name: "xx", Gard: "yy"}, 2: &TBase{Name: "xc", Gard: "yc"}},
+        NMap:    map[TKey]*TBase{TKey{Id: 1, Name: "111"}: &TBase{Name: "xx", Gard: "yy"}, TKey{Id: 2, Name: "222"}: &TBase{Name: "xc", Gard: "yc"}},
         Array:   []int32{1, 2},
         Unkonwn: &TInfo{Name: "honghong", Score: 100},
     }
@@ -38,40 +43,44 @@ func main() {
 
 func Display(name string, x interface{}) {
     fmt.Printf("Display %s (%T):\n", name, x)
-    display(name, reflect.ValueOf(x))
+    cycle := make(map[string]struct{})
+    display(name, reflect.ValueOf(x), cycle)
 }
 
-func display(path string, v reflect.Value) {
+func display(path string, v reflect.Value, cycle map[string]struct{}) {
     switch v.Kind() {
     case reflect.Invalid:
         fmt.Printf("%s = invalid\n", path)
     case reflect.Slice, reflect.Array:
         for i := 0; i < v.Len(); i++ {
-            display(fmt.Sprintf("%s[%d]", path, i), v.Index(i))
+            display(fmt.Sprintf("%s[%d]", path, i), v.Index(i), cycle)
         }
     case reflect.Struct:
         for i := 0; i < v.NumField(); i++ {
-            display(fmt.Sprintf("%s.%s", path, v.Type().Field(i).Name), v.Field(i))
+            if _, ok := cycle[v.Type().Field(i).Name]; ok {
+                continue
+            }
+            cycle[v.Type().Field(i).Name] = struct{}{}
+            display(fmt.Sprintf("%s.%s", path, v.Type().Field(i).Name), v.Field(i), cycle)
         }
     case reflect.Map:
         for _, key := range v.MapKeys() {
-            display(fmt.Sprintf("%s[%v]", path, key), v.MapIndex(key))
+            display(fmt.Sprintf("%s[%v]", path, key), v.MapIndex(key), cycle)
         }
     case reflect.Ptr:
         if v.IsNil() {
             fmt.Printf("%s = nil\n", path)
         } else {
-            display(fmt.Sprintf("(*%s)", path), v.Elem())
+            display(fmt.Sprintf("(*%s)", path), v.Elem(), cycle)
         }
     case reflect.Interface:
         if v.IsNil() {
             fmt.Printf("%s = nil\n", path)
         } else {
             fmt.Printf("%s.type = %s\n", path, v.Elem().Type())
-            display(fmt.Sprintf("%s.value", path), v.Elem())
+            display(fmt.Sprintf("%s.value", path), v.Elem(), cycle)
         }
     default:
         fmt.Printf("%s = %v\n", path, v)
-
     }
 }
