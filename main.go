349d61982b9ee00614efb91412af73b6d5ed59e7
jiarong <15982195424@163.com>
2020-11-18 17:08:17 +0800

    x

--- main.go
+++ main.go
@@ -1,26 +1,7 @@
 package main
 
-import (
-	"fmt"
-	"unsafe"
-)
-
-type Inf struct {
-	ID    int16
-	IsOk1 int32
-}
+import "C"
 
 func main() {
-	s := "h你"
-	for i, v := range s {
-		fmt.Printf("%c, %c\n", s[i], v)
-
-	}
-	fmt.Println("======")
-	for i := 0; i < len(s); i++ {
-		fmt.Printf("%c\n", s[i])
-	}
-
-	var x int
-	fmt.Println(unsafe.Sizeof(x))
+	println("hello cgo")
 }
