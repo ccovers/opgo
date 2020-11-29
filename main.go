6025211fd32ffa8875e6087e46aa42648cf5a5bf
jiarong <15982195424@163.com>
2020-11-29 13:59:50 +0800

    tmp

--- main.go
+++ main.go
@@ -2,10 +2,19 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 	"time"
+
+	"github.com/gorilla/mux"
 )
 
 func main() {
+	r := mux.NewRouter()
+	r.HandleFunc("/", func(http.ResponseWriter, *http.Request) {
+		fmt.Println("hello")
+	})
+	http.Handle("/", r)
+
 	ch := make(chan int, 0)
 	go func() {
 		cnt := 0
