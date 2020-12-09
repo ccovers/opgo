86550736f3c60a512af0440f1dba0cbcdf02e71c
jiarong <15982195424@163.com>
2020-12-09 20:59:09 +0800

    pprof

--- main.go
+++ main.go
@@ -2,39 +2,14 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
-	"time"
-
-	"github.com/gorilla/mux"
+	_ "net/http/pprof" // here be dragons
 )
 
 func main() {
-	r := mux.NewRouter()
-	r.HandleFunc("/", func(http.ResponseWriter, *http.Request) {
-		fmt.Println("hello")
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello World!")
 	})
-	http.Handle("/", r)
-
-	ch := make(chan int, 0)
-	go func() {
-		cnt := 0
-		timer := time.NewTimer(2 * time.Second)
-		for {
-			select {
-			case <-timer.C:
-				fmt.Println("over")
-			default:
-				fmt.Println("xxx")
-				time.Sleep(1 * time.Second)
-				cnt++
-				if cnt > 3 {
-					ch <- 0
-					return
-				}
-				timer.Reset(1 * time.Second)
-			}
-		}
-	}()
-	x := <-ch
-	fmt.Println("...", x)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
